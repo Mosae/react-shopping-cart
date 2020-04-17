83d7e7164f45cd6a37857000862796d98f5738ba
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-17 00:08:46 -0500

    Passed value props to provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import ProductContext from './contexts/ProductContext'; // Next import the product context
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -18,16 +18,18 @@ function App() {
 	console.log(cart);
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider>
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products products={products} addItem={addItem} />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const ProductContext = createContext(); //Step 1: creating product context
+export default ProductContext;
