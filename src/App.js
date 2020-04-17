05392ec898fc04fefc756be20247b40bd0a38e17
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-17 00:56:22 -0500

    starting Step 7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from './contexts/ProductContext'; // Next import the product context
+import CartContext from './contexts/CartContext';
 // Components
+
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
@@ -19,19 +21,19 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				{/* <Route exact path="/">
-  <Products products={products} addItem={addItem} />
-</Route>  Before refactoring */}
-				<Route exact path="/">
-					<Products />
-				</Route>
+					{/* Routes */}
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
